95a471f9e668ae0fd6bcf10afb7b1c6452193c51
Khadijaz8 <158305799+Khadijaz8@users.noreply.github.com>
2025-07-12 22:59:11 +0500

    Update App.py

--- App/App.py
+++ App/App.py
@@ -286,11 +286,11 @@ def pdf_reader(file):
 
 
 # show uploaded file path to view pdf_display
-# def show_pdf(file_path):
-#     with open(file_path, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-#     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 # course recommendations which has data already loaded from Courses.py
